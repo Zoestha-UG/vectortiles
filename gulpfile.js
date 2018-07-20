125d90ffce36a74be8fe6cddc4420909206ec8c0
Sheub <sebastien@sheub.eu>
2018-07-20 22:47:33 +0200

    reGulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,15 @@ var minifyCSS = require('gulp-csso');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 const prettier = require('gulp-prettier');
+const minify = require('gulp-minify');
+
 
 gulp.task('pretty', function(){
   return gulp.src('js/app.js')
     .pipe(prettier({ singleQuote: false }))
     .pipe(gulp.dest('dist/'))
 });
+
 
 gulp.task('css', function(){
   return gulp.src(['vendor/bootstrap/css/bootstrap.min.css', 'css/404.css', 'css/*.css'])
@@ -21,7 +24,8 @@ gulp.task('css', function(){
 gulp.task('js', function(){
   return gulp.src(['js/mapbox-gl-directions.js', 'js/rrssb.min.js','dist/app.js'])
     .pipe(sourcemaps.init())
-    .pipe(concat('app.min.js'))
+	.pipe(minify())
+    .pipe(concat('app.min.js'))	
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('build/js'))
 });
