f3f14c362b70e5280de663d1f93f0af9672aded8
Sheub <sebastien@sheub.eu>
2018-07-20 17:05:57 +0200

    add slach to service worker register path

--- PWAprojects/serviceWorker4/pwabuilder-sw-register.js
+++ PWAprojects/serviceWorker4/pwabuilder-sw-register.js
@@ -6,7 +6,7 @@ if (navigator.serviceWorker.controller) {
 } else {
 
 //Register the ServiceWorker
-  navigator.serviceWorker.register('pwabuilder-sw.js', {
+  navigator.serviceWorker.register('/pwabuilder-sw.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
